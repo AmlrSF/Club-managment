0d7556350d7c2292511f9065195e7a281d45ede2
softyAmir <theyabcpssouaf2@gmail.com>
2024-04-25 10:45:08 -1100

    update & delete posts

--- src/app/site-pages/groupes-posts/groupes-posts.component.ts
+++ src/app/site-pages/groupes-posts/groupes-posts.component.ts
@@ -11,52 +11,44 @@ import { AuthUserService } from 'src/app/services/auth/auth-user.service';
 })
 export class GroupesPostsComponent implements OnInit {
 
+
   groupId: string = "";
   club: any | null = null;
   imageUrl: string | undefined;
   public addpost!: FormGroup;
-  setLoding: any;
+  setLoding: any = false;
   id: any;
   posts: any[] = [];
+  mode: string = "insert";
+  selectedId: any;
 
-  constructor(private auth:AuthUserService,private fb: FormBuilder, private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
+  constructor(private auth: AuthUserService, private fb: FormBuilder, private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
+
+  public formatReadableDate(dateString: any) {
+    const options: any = { year: 'numeric', month: 'long', day: 'numeric' };
+
+    const date = new Date(dateString);
+
+    return date.toLocaleString('en-US', options);
+  }
 
   ngOnInit(): void {
 
-   
 
-    let token = {
-      token: this.auth.getToken()
-    };
+    this.id = this.auth.getId()
+
 
     // console.log(token);
 
-    try {
-      this.http.post(`http://localhost:3000/api/v1/customers/profile`, token).subscribe(
-        (res: any) => {
-          if (!res.success) {
-            this.router.navigate(["Login"]);
-          }else{
-            let url = this.router.url;
+    let url = this.router.url;
 
-            this.fetchClubById(url.split('/')[2]);
-            this.fetchPosts();
-        
-            this.addpost = this.fb.group({
-              content: ['', Validators.required],
-            });
-          }
+    this.groupId = url.split('/')[2];
+    this.fetchClubById(this.groupId);
+    this.fetchPosts();
 
-          
-          this.id = res.customer._id;
-
-        }, (err: any) => {
-          console.log(err);
-        }
-      );
-    } catch (error) {
-      console.log(error);
-    }
+    this.addpost = this.fb.group({
+      content: ['', Validators.required],
+    });
 
 
   }
@@ -99,48 +91,138 @@ export class GroupesPostsComponent implements OnInit {
 
   fetchPosts(): void {
     try {
-      this.setLoding = true;
-      this.http.get(`http://localhost:3000/api/v1/posts/${this.club.ownerId}`)
+      // console.log(this.club)
+      this.http.get(`http://localhost:3000/api/v1/posts/club/${this.groupId}`)
         .subscribe((res: any) => {
           console.log(res);
           this.posts = res.posts;
-          this.setLoding = false;
+
         }, (err: any) => {
           console.error(err);
-          this.setLoding = false;
         });
     } catch (error) {
       console.error(error);
     }
   }
-  
+
 
   submitForm() {
-    if (this.addpost.valid) {
-      this.addpost.value['imageUrl'] = this.imageUrl;
-      try {
-        this.setLoding = true;
-        let postInfo:any = {
-          ...this.addpost.value,
-          author:this.id,
-          club:this.club.ownerId._id
-        }
-
-        console.log(postInfo);
-        
-        this.http.post('http://localhost:3000/api/v1/posts',postInfo)
-          .subscribe((res:any)=>{
-            console.log(res);
-
-            this.setLoding = false
-          },(err:any)=>{
+    if (this.addpost.valid && this.imageUrl) {
+      if(this.mode=="update"){
+        try {
+          this.setLoding=true;
+          let updatePostObj :any={
+            ...this.addpost.value,
+            imageUrl: this.imageUrl
+          }
+          this.http.put(`http://localhost:3000/api/v1/posts/${this.selectedId}`,updatePostObj)
+          .subscribe((res: any) => {
+            this.setLoding=false;
+            this.addpost.reset();
+            this.imageUrl="";
+            this.mode="insert";
+            this.fetchPosts();
+            
+            
+          }, (err: any) => {
             console.log(err)
           })
-      } catch (error) {
-
+        } catch (error) {
+          console.log(error);
+          
+        }
+      }else{
+        this.addpost.value['imageUrl'] = this.imageUrl;
+        try {
+          this.setLoding = true;
+          let postInfo: any = {
+            ...this.addpost.value,
+            author: this.id,
+            club: this.groupId
+          }
+  
+          console.log(postInfo);
+          this.setLoding = true;
+          this.http.post('http://localhost:3000/api/v1/posts', postInfo)
+            .subscribe((res: any) => {
+              console.log(res);
+  
+              this.setLoding = false
+            }, (err: any) => {
+              console.log(err)
+            })
+        } catch (error) {
+          console.log("error")
+        }
       }
     } else {
       alert("fill in field");
     }
   }
+
+  downVote(id:any) {
+    try {
+      this.http.patch(`http://localhost:3000/api/v1/posts/${id}/downvote`,{id:this.id})
+      .subscribe((res: any) => {
+        console.log(res);
+        this.fetchPosts();
+      }, (err: any) => {
+        console.log(err)
+      })
+    } catch (error) {
+      
+    }
+  }
+  upVote(id:any) {
+    try {
+      this.http.patch(`http://localhost:3000/api/v1/posts/${id}/upvote`,{id:this.id})
+      .subscribe((res: any) => {
+        console.log(res);
+        this.fetchPosts();
+      }, (err: any) => {
+        console.log(err)
+      })
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  public deletePost(id:any){
+    try {
+      this.http.delete(`http://localhost:3000/api/v1/posts/${id}`)
+      .subscribe((res: any) => {
+        console.log(res);
+        this.fetchPosts();
+      }, (err: any) => {
+        console.log(err)
+      })
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  
+  public updatePost(id:any){
+    try {
+      this.http.get(`http://localhost:3000/api/v1/posts/${id}`)
+      .subscribe((res: any) => {
+        this.addpost.patchValue({
+          content:res.post.content
+       });
+       this.imageUrl = res?.post.imageUrl;
+
+       this.mode = "update";
+       this.selectedId = id;
+        
+      }, (err: any) => {
+        console.log(err)
+      })
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
 }
