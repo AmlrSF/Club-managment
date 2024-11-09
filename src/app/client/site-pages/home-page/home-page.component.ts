c1e5ea670d384f791f0b7101b00eb95e06ef33a2
softyAmir <theyabcpssouaf2@gmail.com>
2024-11-08 22:35:06 -1100

    post details page  and comment section

--- src/app/client/site-pages/home-page/home-page.component.ts
+++ src/app/client/site-pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthUserService } from 'src/app/services/auth/auth-user.service';
 
 @Component({
@@ -25,7 +26,7 @@ export class HomePageComponent implements OnInit{
   }
   
 
-  constructor(private http: HttpClient,private auth:AuthUserService) { }
+  constructor(private http: HttpClient,private router:Router,private auth:AuthUserService) { }
 
 
   fetchPosts(): void {
@@ -81,57 +82,7 @@ export class HomePageComponent implements OnInit{
     }
   }
 
-  
-  // public create() {
-  //   this.http.post(this.baseUrm, this.formData, { headers: this.headers }).subscribe(
-  //     (res: any) => {
-  //       console.log(res);
-
-  //       // Check if the response status is true
-  //       if (res.status === true) {
-  //         // Redirect the user to the payment URL
-  //         window.location.href = res.data.payment_url;
-  //       }
-  //     },
-  //     (err: any) => {
-  //       console.log(err);
-  //     }
-  //   );
-  // }
-  // startUpdatingStats() {
-  //   // Update the statistics at regular intervals
-  //   setInterval(() => {
-  //     // Simulate an API call to get the updated statistics
-  //     // Replace this with your actual API call logic
-  //     this.clients = Math.min(this.clients + 1, 150);
-  //     this.projects = Math.min(this.projects + 1, 135);
-  //     this.countries = Math.min(this.countries + 1, 50);
-  //     this.money = Math.min(this.money + 1, 500);
-  //   }, 1); // Update every 1000 milliseconds (1 second)
-  // }
-
-
-  // private baseUrm = "https://sandbox.paymee.tn/api/v2/payments/create";
-
-  // private formData = {
-  //   "amount": 220.25,
-  //   "note": "Order #123",
-  //   "first_name": "John",
-  //   "last_name": "Doe",
-  //   "email": "test@paymee.tn",
-  //   "phone": "+21611222333",
-  //   "return_url": "https://www.return_url.tn",
-  //   "cancel_url": "https://www.cancel_url.tn",
-  //   "webhook_url": "https://www.webhook_url.tn",
-  //   "order_id": "244557"
-  // }
-
-  // private apiKey = 'e430b12a45aa846ebc21395a35d43956cae176a1';  // Replace with your actual API key
-
-  // // Set up the headers with the authorization token
-  // private headers = new HttpHeaders({
-  //   'Authorization': `Token ${this.apiKey}`,
-  //   'Content-Type': 'application/json'
-  // });
-
+  public navigateToPostDetail(id:any){
+    this.router.navigate([`/posts/post/${id}`])
+  }
 }
--- src/app/client/site-pages/post/post-details/post-details.component.ts
+++ src/app/client/site-pages/post/post-details/post-details.component.ts
@@ -1,10 +1,138 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthUserService } from 'src/app/services/auth/auth-user.service';
 
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
   styleUrls: ['./post-details.component.css']
 })
-export class PostDetailsComponent {
+export class PostDetailsComponent implements OnInit {
+  public customer: any;
+  public squad: any; // Variable to store the squad details
+  post: any;
+  id: any;
+  commentForm!: FormGroup;
 
+  constructor(
+    private auth: AuthUserService,
+    private router: Router,
+    private route: ActivatedRoute, // Inject ActivatedRoute
+    private http: HttpClient,
+    private fb:FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    let token = { token: this.auth.getToken() };
+    this.id = this.auth.getId();
+
+    this.commentForm = this.fb.group({
+      content: ['', [Validators.required, Validators.minLength(1)]]
+    });
+
+    // Check if user is authenticated
+    this.http
+      .post(`http://localhost:3000/api/v1/customers/profile`, token)
+      .subscribe(
+        (res: any) => {
+          if (!res.success) {
+            this.router.navigate(['Login']);
+          }
+          this.customer = res.customer;
+        },
+        (err: any) => {
+          console.log(err);
+        }
+      );
+
+
+    this.fetchPost();
+  }
+
+  onSubmit(): void {
+    if (this.commentForm.valid) {
+      const commentContent = this.commentForm.value.content;
+      console.log('Comment submitted:', commentContent);
+      this.commentForm.reset();  // Clear the form after submission
+    }
+  }
+
+  truncateText(text: string, maxLength: number = 100): string {
+    return text.length > maxLength
+      ? text.substring(0, maxLength) + '...'
+      : text;
+  }
+
+  fetchPost(): void {
+    const squadId = this.route.snapshot.paramMap.get('id');
+    try {
+      // console.log(this.club)
+      this.http.get(`http://localhost:3000/api/v1/Posts/${squadId}`).subscribe(
+        (res: any) => {
+          this.post = res.post
+          console.log(res);
+          
+        },
+        (err: any) => {
+          console.error(err);
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  public formatReadableDate(dateString: any) {
+    const options: any = { year: 'numeric', month: 'long', day: 'numeric' };
+
+    const date = new Date(dateString);
+
+    return date.toLocaleString('en-US', options);
+  }
+
+  downVote(id: any) {
+    try {
+      this.http
+        .patch(`http://localhost:3000/api/v1/Post/${id}/downvote`, {
+          id: this.id,
+        })
+        .subscribe(
+          (res: any) => {
+            console.log(res);
+            this.fetchPost();
+          },
+          (err: any) => {
+            console.log(err);
+          }
+        );
+    } catch (error) {}
+  }
+  upVote(id: any) {
+    try {
+      this.http
+        .patch(`http://localhost:3000/api/v1/Post/${id}/upvote`, {
+          id: this.id,
+        })
+        .subscribe(
+          (res: any) => {
+            console.log(res);
+            this.fetchPost();
+          },
+          (err: any) => {
+            console.log(err);
+          }
+        );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
+
+  public navigateToManage(squad:any){
+    this.router.navigate([`/squads/squad-details/${squad._id}`])
+  }
 }
