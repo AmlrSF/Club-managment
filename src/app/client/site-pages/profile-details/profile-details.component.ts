e0693273b6a2707eedb8ab79a17f1b2a4735098f
softyAmir <theyabcpssouaf2@gmail.com>
2024-11-23 10:16:10 -1100

    detils page

--- src/app/client/site-pages/profile-details/profile-details.component.ts
+++ src/app/client/site-pages/profile-details/profile-details.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthUserService } from 'src/app/services/auth/auth-user.service';
 
 @Component({
   selector: 'app-profile-details',
   templateUrl: './profile-details.component.html',
   styleUrls: ['./profile-details.component.css']
 })
-export class ProfileDetailsComponent {
+export class ProfileDetailsComponent  implements OnInit{
+  customer: any;
+
+  constructor(
+    private auth: AuthUserService,
+    private http: HttpClient,
+    private router: Router,
+    private fb: FormBuilder,
+    private route : ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+
+    try {
+      this.http.get(`http://localhost:3000/api/v1/customers/${id}`,).subscribe(
+        (res: any) => {
+          if (!res.success) {
+            this.router.navigate(["Login"]);
+          }
+          console.log(res);
+          this.customer = res.customer;
+       
+        }, (err: any) => {
+          console.log(err);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 }
