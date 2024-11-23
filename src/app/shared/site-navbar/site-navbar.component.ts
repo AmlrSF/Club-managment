8b11f9de15268cb2f402ba9cbd1073fa7e1f0b46
softyAmir <theyabcpssouaf2@gmail.com>
2024-11-23 10:17:29 -1100

    navbar fix

--- src/app/shared/site-navbar/site-navbar.component.ts
+++ src/app/shared/site-navbar/site-navbar.component.ts
@@ -8,65 +8,64 @@ import { FuncServicesService } from 'src/app/services/func-services.service';
 @Component({
   selector: 'app-site-navbar',
   templateUrl: './site-navbar.component.html',
-  styleUrls: ['./site-navbar.component.css']
+  styleUrls: ['./site-navbar.component.css'],
 })
-export class SiteNavbarComponent  implements OnInit{
+export class SiteNavbarComponent implements OnInit {
+  public customer: any = null;
 
+  public constructor(
+    private http: HttpClient,
+    private auth: AuthUserService,
+    private functionsS: FuncServicesService,
+    private router: Router
+  ) {}
 
-  public customer:any = null;
-
-  public constructor(private http:HttpClient, 
-    private auth:AuthUserService,private functionsS:FuncServicesService,
-     private router:Router ){}
-
+  navigateToProfileDetails(id: any) {
+    this.router.navigate([`/profile/${id}`]);
+  }
 
   ngOnInit(): void {
-   let token = {
-      token: this.auth.getToken()
+    let token = {
+      token: this.auth.getToken(),
     };
 
     console.log(token);
 
     try {
-      this.http.post(`http://localhost:3000/api/v1/customers/profile`, token).subscribe(
-        (res: any) => {
-          
-
-         
-          this.customer = res.customer;
-          console.log(this.customer);
-          
-
-        }, (err: any) => {
-          console.log(err);
-        }
-      );
+      this.http
+        .post(`http://localhost:3000/api/v1/customers/profile`, token)
+        .subscribe(
+          (res: any) => {
+            this.customer = res.customer;
+            console.log(this.customer);
+          },
+          (err: any) => {
+            console.log(err);
+          }
+        );
     } catch (error) {
       console.log(error);
     }
   }
 
-  public logout(){
+  public logout() {
     this.toggleDropdown();
     return this.auth.logout();
   }
-  
+
   public toggleDropdown() {
     document.getElementById('user-dropdown')?.classList.toggle('hidden');
   }
 
   public navigateTo(item: string) {
-
     this.router.navigate([item]);
-
   }
-  
-  
-  public addToggle(){
-    this.functionsS.addToggle(); 
+
+  public addToggle() {
+    this.functionsS.addToggle();
   }
 
   navigateToGroupe() {
-    this.router.navigate(["/posts/new"]);
+    this.router.navigate(['/posts/new']);
   }
 }
