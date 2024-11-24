5906cb0d31bd70dd4491e1ea4ff31e8507f5ad0b
softyAmir <theyabcpssouaf2@gmail.com>
2024-11-24 08:55:58 -1100

    squad page for admin control

--- src/app/admin/pages/squads/squads.component.ts
+++ src/app/admin/pages/squads/squads.component.ts
@@ -4,22 +4,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-squads',
   templateUrl: './squads.component.html',
-  styleUrls: ['./squads.component.css']
+  styleUrls: ['./squads.component.css'],
 })
 export class SquadsComponent implements OnInit {
-deleteSquad(_t33: any) {
-throw new Error('Method not implemented.');
-}
-editSquad(_t33: any) {
-throw new Error('Method not implemented.');
-}
-viewSquad(_t33: any) {
-throw new Error('Method not implemented.');
-}
-  private apiUrl:string = "http://localhost:3000/api/v1/clubs"
+  private apiUrl: string = 'http://localhost:3000/api/v1/clubs';
   squads: any[] = [];
   filteredSquads: any[] = [];
-  public constructor(private http : HttpClient){}
+  public constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getClients();
@@ -28,7 +19,7 @@ throw new Error('Method not implemented.');
   getClients(): void {
     this.http.get(this.apiUrl).subscribe(
       (clients: any) => {
-        console.log(clients)
+        console.log(clients);
         this.squads = clients.clubs;
         this.filteredSquads = [...this.squads];
         console.log('Clients:', this.squads);
@@ -41,22 +32,45 @@ throw new Error('Method not implemented.');
 
   public formatReadableDate(dateString: any) {
     const options: any = { year: 'numeric', month: 'long', day: 'numeric' };
-  
+
     const date = new Date(dateString);
-  
+
     return date.toLocaleString('en-US', options);
   }
-  
+
   applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
-    this.filteredSquads = this.squads.filter(squad =>
-      squad.name.toLowerCase().includes(filterValue) ||
-      squad.genre.toLowerCase().includes(filterValue) ||
-      squad.description.toLowerCase().includes(filterValue) ||
-      `${squad.ownerId.firstName} ${squad.ownerId.lastName}`.toLowerCase().includes(filterValue) ||
-      squad.createdAt.toLowerCase().includes(filterValue)
+    this.filteredSquads = this.squads.filter(
+      (squad) =>
+        squad.name.toLowerCase().includes(filterValue) ||
+        squad.genre.toLowerCase().includes(filterValue) ||
+        squad.description.toLowerCase().includes(filterValue) ||
+        `${squad.ownerId.firstName} ${squad.ownerId.lastName}`
+          .toLowerCase()
+          .includes(filterValue) ||
+        squad.createdAt.toLowerCase().includes(filterValue)
     );
   }
-  
 
+  deleteSquad(squad: any) {
+    this.http.delete(`${this.apiUrl}/${squad._id}`).subscribe(
+      (clients: any) => {
+        this.getClients();
+      },
+      (error: any) => {
+        console.error('Error fetching clients:', error);
+      }
+    );
+  }
+
+  ApproveSquad(squad: any) {
+    this.http.put(`${this.apiUrl}/${squad._id}`, {approved : !squad.approved}).subscribe(
+      (clients: any) => {
+        this.getClients();
+      },
+      (error: any) => {
+        console.error('Error fetching clients:', error);
+      }
+    );
+  }
 }
